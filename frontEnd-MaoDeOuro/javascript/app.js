c6e045ebb9b02d2a48d6119d439f2cc0875e26be
Paulo Felipe <p.felipe@msn.com>
2017-08-11 14:35:28 -0300

    Arquitetura do projeto resolvido

--- frontEnd-MaoDeOuro/javascript/app.js
+++ frontEnd-MaoDeOuro/javascript/app.js
@@ -1,31 +1,35 @@
 var app = angular.module('starter',['ngRoute', 'starter.controllers']);
 
-app.config(function($routeProvider)
+app.config(function($routeProvider, $locationProvider)
 {
+    // remove o # da url
+    $locationProvider.html5Mode({
+     enabled: true,
+     requireBase: false
+    });
 
    $routeProvider
-
    // para a rota '/', carregaremos o template home.html e o controller 'HomeCtrl'
    .when('/', {
-      templateUrl : 'home.html',
+      templateUrl : 'views/home.html',
       controller  : 'HomeController',
    })
 
    // para a rota '/sobre', carregaremos o template sobre.html e o controller 'SobreCtrl'
    .when('/sobre', {
-      templateUrl : 'sobre.html',
+      templateUrl : 'views/sobre.html',
       controller  : 'SobreController',
    })
 
    // para a rota '/promocao', carregaremos o template promocao.html e o controller 'SobreCtrl'
    .when('/promocao', {
-      templateUrl : 'promocao.html',
+      templateUrl : 'views/promocao.html',
       controller  : 'PromocaoController',
    })
 
    // para a rota '/contato', carregaremos o template contato.html e o controller 'ContatoCtrl'
    .when('/contato', {
-      templateUrl : 'contato.html',
+      templateUrl : 'views/contato.html',
       controller  : 'ContatosController',
    })
 
